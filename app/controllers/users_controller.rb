04307443dcf05f2ff63c3ba10a70a358c1e93669
EC2 Default User <ec2-user@ip-172-31-8-248.ec2.internal>
2020-11-24 05:49:06 +0000

    マイページ機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @user = current_user
     @movie = Movie.new
     @movies = @user.movies.page(params[:page]).per(4).reverse_order
+    @comment = @user.comments.page(params[:page]).per(4).reverse_order
+    @all = Movie.all
   end
 
   private
